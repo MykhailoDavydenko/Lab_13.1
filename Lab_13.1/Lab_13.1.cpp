a8295e7b7de1bcc502b868d4c928ec6b38f13019
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-11-18 10:49:47 +0200

    first commit

---
+++ Lab_13.1/Lab_13.1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include "dod.h"
+#include "sum.h"
+#include "var.h"
+using namespace std;
+using namespace nsDod;
+using namespace nsSum;
+using namespace nsVar;
+int main()
+{
+	double Pi = 4 * atan(1.0);
+	cout << "x_p = "; cin >> x_p;
+	cout << "x_k = "; cin >> x_k;
+	cout << "dx = "; cin >> dx;
+	cout << "e = "; cin >> e;
+	cout << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(12) << "arcctan(x)" << " |"
+		<< setw(10) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-----------------------------------------" << endl;
+	x = x_p;
+	while (x <= x_k)
+	{
+		sum(); // виклик процедури обчислення суми
+		if (x > 0)
+			arcctg = atan(1. / x);
+		else
+			arcctg = Pi + atan(1 / x);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << arcctg << " |"
+			<< setw(10) << setprecision(5) << Pi / 2 + s << " |"
+			<< setw(5) << n << " |"
+			<< endl; // тут слід використовувати форматний вивід
+		x += dx;
+		
+	}
+	cout << "-----------------------------------------" << endl;
+	cin.get();
+	return 0;
+}
